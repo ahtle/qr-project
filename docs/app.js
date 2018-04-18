c6e04e9825a4780800ccedd573dd53b60dc52b85
Anh T Le <ahtle@stanford.edu>
2018-04-18 16:50:46 -0700

    stop video button

--- docs/app.js
+++ docs/app.js
@@ -3,25 +3,35 @@ $('form').on('submit', function(e) {
     let name = $('#name').val();
     console.log(name);
 
-    $('#get-qr').attr('src', 'https://api.qrserver.com/v1/create-qr-code/?data=' + name + '&size=200x200');
+    $('#get-qr').attr('src', 'https://api.qrserver.com/v1/create-qr-code/?data=' + name);
 });
 
+// scanner
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+
+scanner.addListener('scan', function (content) {
+    console.log(content);
+    $('#scan-result').text(content);
+});
 
 $('#start-scan').click(function() {
-    let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-    
-    scanner.addListener('scan', function (content) {
-        console.log(content);
-    });
     
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
             scanner.start(cameras[0]);
         } else {
-            console.error('No cameras found.');
+            $('#scan-result').text('No camera found.');
         }
         }).catch(function (e) {
-        console.error(e);
-    });
+            console.error(e);
+        });
+
+});
+
+$('#stop-scan').click(function() {
+    scanner.stop();
+});
+
+$('#capture-image').click(function(){
 
 });
